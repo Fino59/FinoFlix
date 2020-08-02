1e030c55e2728aeffa9167e3101b12fc1a2e408a
Fino59 <slipdan@hotmail.com>
2020-08-02 18:28:05 -0300

    Atualização da aula 2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Menu from './components/Menu';
-import dadosIniciais from './components/Menu/data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu';
+import dadosIniciais from '../../components/Menu/data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
 
-function App() {
+function Home() {
   return (
     <div style={{ background: "#141414"}}>
       <Menu />
@@ -14,7 +14,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription={"Contra Baixo é facil e chato? Guitarras são mais dificeis e mais divertidas? Descubra agora."}
       />
 
       <Carousel 
@@ -48,4 +48,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
