497be653d9fd62690e3344d08cc497ef7ed5b2f8
Danilo Fontes <slipdan@hotmail.com>
2021-08-18 23:55:56 -0300

    Update with Fetch Api, useState and useEffect

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
@@ -8,7 +8,7 @@ import Button from '../../../components/Button';
 function CadastroCategoria() {
   const valoresIniciais = {
     nome: '',
-    descriçao: '',
+    descricao: '',
     cor: '',
   };
 
@@ -28,6 +28,18 @@ function CadastroCategoria() {
       infosDoEvento.target.value,
     );
   }
+
+  useEffect(() => {
+    const URL_API = 'http://localhost:4000/categorias';
+
+    fetch(URL_API)
+      .then(async (respostaDoServidor) => {
+        const resposta = await respostaDoServidor.json();
+        setCategorias([
+          ...resposta,
+        ]);
+      });
+  }, []);
 
   return (
     <PageDefault>
@@ -75,6 +87,12 @@ function CadastroCategoria() {
           Cadastrar
         </Button>
       </form>
+
+      {categorias.length === 0 && (
+        <div>
+          Loading...
+        </div>
+      )}
 
       <ul>
         {categorias.map((categoria) => (
