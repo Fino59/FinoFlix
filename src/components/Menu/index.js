2f962af747eaa8828f61ba2f325029604de05da0
Danilo Fontes <slipdan@hotmail.com>
2021-08-25 22:31:47 -0300

    Update with padding for main page

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/imgs/Logo.png';
 import './Menu.css';
-import Button from '../Button'
-//import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <Link to="/">
-            <img className="Logo" src={Logo} alt="Fino Flix Logo"/>
-            </Link>
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="Fino Flix Logo" />
+      </Link>
 
-            <Button as={Link} className="ButtonLink" to="/cadastro/video">
-            Novo Vídeo
-            </Button>
-        </nav>
-    );
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo Vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
